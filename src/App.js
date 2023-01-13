a02840514168df51daeb3171e807b437e70672e2
imdeva91 <113455884+imdeva91@users.noreply.github.com>
2023-01-13 21:54:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div >
+       
+        <img className='img' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="" />
+        </div>
+        <div className='span'>
+        <span>SOME RANDOM TEXT, SOME RANDOM TEXT,SOME RANDOM TEXT,SOME RANDOM TEXT</span>
+        </div>
+
+             {/* ..............Form......... */}
+
+        <div className='login-signup'>
+          <span className='login'>Login</span>
+          <span className='signup'>Signup</span>
+        </div>
+        <div className='path'></div>
+
+        <div>
+          <input className='email' type="text" placeholder="Email Address" />
+        </div>
+        <div >
+          <input className='password' type="password" placeholder="Password" />
+        </div>
+        <button>LOGIN</button>
+        <div className='forgot'>
+          <span >Forgot Password?</span>
+        </div>
+        <div>
+          <p className='or-login'>or login with</p>
+        </div>
+      
+      {/* ........social media....... */}
+
+        <div className='social-media'>
+        <img className='google' alt='google' src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png' />
+        <img className='facebook'  alt='facebook' src='https://www.freeiconspng.com/thumbs/facebook-logo-png/facebook-f-logo-transparent-facebook-f-22.png'/>
+        <img className='twitter'  alt='twitter' src='https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png'/>
+
+        </div>
+
+        {/* footer................. */}
+        <div className='account'>
+          <span className='have-an-account'>Don’t have an account?</span><span className='create'>Create new now!</span>
+        </div>
+        <div className='terms'>
+          <span className='agree'> By signing up, you are agree with our </span><span className='term-condition'>Terms & Conditions</span>
+        </div>
     </div>
   );
 }
